dc8dd5ed82cfa0e2151c28649b352de9ace9899a
agusp-dev <agustin.perez@esolutions.com.ar>
2021-06-20 22:31:20 -0300

    Integrated react-query

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
-import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { useQuery } from 'react-query'
 
 export default function App() {
 
-  const [isLoading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
-  const [data, setData] = useState({})
+  // const [isLoading, setLoading] = useState(false)
+  // const [error, setError] = useState(false)
+  // const [data, setData] = useState({})
 
-  useEffect( () => {
+  const { 
+    isLoading, 
+    error, 
+    data 
+  } = useQuery('dogs', () => axios('https://random.dog/woof.json'))
 
-    const fetchData = async () => {
-      setData(null)
-      setError(false)
-      setLoading(true)
-      try {
-        const response = await axios('https://random.dog/woof.json')
-        setData(response.data) 
-      } catch (error) {
-        setError(true)
-      }
-      setLoading(false)
-    }
-    fetchData()
-  }, [])
+  // useEffect( () => {
+  //   const fetchData = async () => {
+  //     setData(null)
+  //     setError(false)
+  //     setLoading(true)
+  //     try {
+  //       const response = await axios('https://random.dog/woof.json')
+  //       setData(response.data) 
+  //     } catch (error) {
+  //       setError(true)
+  //     }
+  //     setLoading(false)
+  //   }
+  //   fetchData()
+  // }, [])
 
   return (
-    <div className="App">
+    <div className='App'>
       {isLoading && (
         <h1>Loading...</h1>
       )}
       {error && (
-        <h1>Error: {error}</h1>
+        <h1>Error: {error.message}</h1>
       )}
-      {data && (
+      {data && data.data && data.data.url && (
         <img 
-          src={data.url} 
+          src={data.data.url} 
           alt='Dog'
           width='800px'
           height='800px' />
